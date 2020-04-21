74bafe5100d5fd3ddc386fcb0dc41957bf6dc0a2
Takahashi Noriyuki <takahashi-n@chwd.ch-mk.jp>
2020-04-21 16:03:35 +0900

    子エレメント

--- src/App.js
+++ src/App.js
@@ -3,11 +3,7 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  data = [
-    "This is list sample",
-    "これはリストのサンプルです。",
-    "配列をリストに変換します。"
-  ];
+  input = '';
 
   msgStyle = {
     fontSize:"20pt",
@@ -19,62 +15,61 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      list:this.data
+      message:'type your name:'
     };
+    this.doChange = this.doChange.bind(this);
+    this.doSubmit = this.doSubmit.bind(this);
+  }
+
+  doChange(event) {
+    this.input = event.target.value;
+  }
+
+  doSubmit(event) {
+    this.setState({
+      message: 'Hello. ' + this.input + '!!'
+    });
+    event.preventDefault();
   }
 
   render(){
     return <div>
       <h1>React</h1>
       <h2 style={this.msgStyle}>show list.</h2>
-      <List title="サンプル・リスト" data={this.data} />
+      <Message title="Children!">
+        これはコンポーネント内のコンテンツです。
+        まるでテキストを分割し、リストにして表示します。
+        改行は必要ありません。
+      </Message>  
     </div>;
   }
 }
 
-class List extends Component {
-  number = 1;
-
-  title = {
+class Message extends Component {
+  li = {
     fontSize:"20pt",
-    fontWeight:"bold",
-    color:"blue",
+    color:"#900",
+    margin:"0px",
+    padding:"0px"
   };
 
   render() {
-    let data = this.props.data;
+    let content = this.props.children;
+    let arr = content.split('。');
+    let arr2 = [];
+    for(let i = 0;i < arr.length;i++){
+      if (arr[i].trim() !== ''){
+        arr2.push(arr[i]);
+      }
+    }
+    let list = arr2.map((value,key) => (
+      <li style={this.li} key={key}>{value}.</li>
+    ))
     return (
       <div>
-        <p style={this.title}>{this.props.title}</p>
-        <ul>
-          {data.map((item) => 
-            <Item number={this.number++} value={item} key={this.number} />
-          )}
-        </ul>
+        <h2>{this.props.title}</h2>
+        <ol>{list}</ol>
       </div>
-    )
-  }
-}
-
-class Item extends Component {
-  li = {
-    listStyleType:"square",
-    fontSize:"16pt",
-    color:"#06",
-    margin:"0px",
-    padding:"0px",
-  }
-  num = {
-    fontWeight:"bold",
-    color:"red"
-  }
-
-  render() {
-    return (
-      <li style={this.li}>
-        <span style={this.num}>[{this.props.number}]</span>
-        {this.props.value}
-      </li>
     )
   }
 }

5e86a5fd7919e1f9a1c905fdb84058a87c16058a
Takahashi Noriyuki <takahashi-n@chwd.ch-mk.jp>
2020-04-21 17:47:23 +0900

    Provider

--- src/App.js
+++ src/App.js
@@ -7,10 +7,17 @@ let data = {title:'Title', message:'this is sample message.'};
 const SampleContext = React.createContext(data);
 
 class App extends Component {
+  newdata = {title:'新しいタイトル', message:'これは新しいメッセージです。'};
 
   render(){
     return <div>
       <h1>React</h1>
+      <Title />
+      <Message />
+      <SampleContext.Provider value={this.newdata}>
+        <Title />
+        <Message />        
+      </SampleContext.Provider>
       <Title />
       <Message />
     </div>;

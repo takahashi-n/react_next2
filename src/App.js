faf29bd01eff64590c0af873962af8af80a515b9
Takahashi Noriyuki <takahashi-n@chwd.ch-mk.jp>
2020-04-21 17:39:34 +0900

    コンテキスト

--- src/App.js
+++ src/App.js
@@ -2,58 +2,41 @@ import React, { Component } from 'react';
 //import Rect from './Rect';
 import './App.css';
 
+let data = {title:'Title', message:'this is sample message.'};
+
+const SampleContext = React.createContext(data);
+
 class App extends Component {
-  input = '';
-
-  msgStyle = {
-    fontSize:"20pt",
-    color:"#900",
-    margin:"20px 0px",
-    padding:"5px"
-  }
-
-  constructor(props){
-    super(props);
-    this.state = {
-      message:'type your name:'
-    };
-    this.doCheck = this.doCheck.bind(this);
-  }
-
-  doCheck(event) {
-    alert(event.target.value + "は長すぎます。（最大10文字）")
-  }
 
   render(){
     return <div>
       <h1>React</h1>
-      <h2>{this.state.message}</h2>
-      <Message maxlength="10" onCheck={this.doCheck} />
+      <Title />
+      <Message />
     </div>;
   }
 }
 
+class Title extends Component {
+  static contextType = SampleContext;
+
+  render(){
+    return (
+      <div>
+        <h2>{this.context.title}</h2>
+      </div>
+    )
+  }
+}
+
 class Message extends Component {
-  inputStyle = {
-    fontSize:"12pt",
-    padding:"5px"
-  }
-
-  constructor(props){
-    super(props);
-    this.doChange = this.doChange.bind(this);
-  }
-
-  doChange(e) {
-    if(e.target.value.length > this.props.maxlength){
-      this.props.onCheck(e);
-      e.target.value = e.target.value.substr(0,this.props.maxlength);
-    }
-  }
+  static contextType = SampleContext;
 
   render() {
     return (
-      <input type="text" style={this.inputStyle} onChange={this.doChange} />
+      <div>
+        <p>{this.context.message}</p>
+      </div>
     )
   }
 }

17e8af35af1bc9de9a27a008f1ca66ebeab6f8aa
Takahashi Noriyuki <takahashi-n@chwd.ch-mk.jp>
2020-04-21 13:42:50 +0900

    プロジェクトでコンポーネント

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Rect from './Rect';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super();
+    this.title = props.title;
+    this.message = props.message;
+  }
+  render(){
+    return <div>
+      <h1>{this.title}</h1>
+      <p>{this.message}</p>
+      <Rect x="50" y="50" w="150" h="150" c="#6ff9" r="50" />            
+      <Rect x="150" y="100" w="150" h="150" c="#f6f9" r="75" />            
+      <Rect x="100" y="150" w="150" h="150" c="#6669" r="25" />           
+    </div>;
+  }
 }
 
 export default App;

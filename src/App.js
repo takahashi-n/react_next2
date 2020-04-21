95f11206831f7feee7e6c82cf70a72e46ee3f719
Takahashi Noriyuki <takahashi-n@chwd.ch-mk.jp>
2020-04-21 14:44:02 +0900

    ステート（真偽）

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 
 class App extends Component {
 
-  msgStyle = {
+  msgStyle1 = {
     fontSize:"24pt",
     color:"#900",
     margin:"20px 0px",
@@ -12,9 +12,18 @@ class App extends Component {
     borderBottom:"2px solid #900"
   }
 
-  btnStyle = {
+  msgStyle2 = {
     fontSize:"20pt",
-    padding:"0px 10px"
+    color:"white",
+    backgroundColor:"#900",
+    margin:"20px 0px",
+    padding:"5px",
+    borderBottom:"2px solid #900"
+  }
+
+  btnStyle = {
+    fontSize:"16pt",
+    padding:"10px"
   }
 
   constructor(props){
@@ -22,6 +31,7 @@ class App extends Component {
     this.state = {
       counter: 0,
       msg:'count start!',
+      flg:true,
     };
     this.doAction = this.doAction.bind(this);
   }
@@ -29,14 +39,19 @@ class App extends Component {
   doAction(e) {
     this.setState((state) => ({
       counter: state.counter + 1,
-      msg: 'count: ' + state.counter
+      msg: state.counter,
+      flg: !state.flg
     }));
   }
 
   render(){
     return <div>
       <h1>React</h1>
-      <p style={this.msgStyle}>{this.state.msg}</p>
+      {this.state.flg ?
+        <p style={this.msgStyle1}>count: {this.state.msg}</p>
+      :
+        <p style={this.msgStyle2}>{this.state.msg}です。</p>
+      }
       <button style={this.btnStyle} onClick={this.doAction}>Click</button>
     </div>;
   }

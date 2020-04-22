324ec8cab480c9efda53484c9d2ffa1313769fa6
Takahashi Noriyuki <takahashi-n@chwd.ch-mk.jp>
2020-04-22 15:19:04 +0900

    Persist

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import './App.css';
-import Memo from './memo/Memo';
-import AddForm from './memo/AddForm';
-import FindForm from './memo/FindForm';
-import DelForm from './memo/DelForm';
 
 class App extends Component {
-  td = {
-    width:"250px"
-  }
 
   constructor(props) {
     super(props);
@@ -18,17 +11,67 @@ class App extends Component {
   render(){
     return (
       <div>
-        <h1>Memo</h1>
-        <AddForm />
-        <hr />
-        <table><tbody><tr>
-        <td style={this.td}><FindForm /></td>
-        <td style={this.td}><DelForm /></td>        
-        </tr></tbody></table>
-        <Memo />
+        <h1>Redux</h1>
+        <Message />
+        <Button />
       </div>
     )
   }
 }
 
-export default connect()(App);
+// ストアのコネクト
+App = connect()(App);
+
+class Message extends Component {
+  style = {
+    fontSize:"20pt",
+    padding:"20px 5px"
+  }
+
+  render() {
+    return (
+      <p style={this.style}>
+        {this.props.message}: {this.props.counter}
+      </p>
+    )
+  }
+}
+
+// ストアのコネクト
+Message = connect((state)=>state)(Message);
+
+class Button extends Component {
+  style = {
+    fontSize:"16pt",
+    padding:"5px 10px"
+  }
+
+  constructor(props) {
+    super(props);
+    this.doAction = this.doAction.bind(this);
+  }
+
+  // ボタンクリックでディスパッチ実行
+  doAction(e) {
+    if (e.shiftKey) {
+      this.props.dispatch({ type:'DECREMENT' });
+    } else if (e.ctrlKey) {
+      this.props.dispatch({ type:'RESET' });
+    } else {
+      this.props.dispatch({ type:'INCREMENT' });
+    }
+  }
+
+  render(){
+    return (
+      <button style={this.style} onClick={this.doAction}>
+        click
+      </button>
+    )
+  }
+}
+
+// ストアのコネクト
+Button = connect()(Button);
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,60 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { PersistGate } from 'redux-persist/integration/react';
 import './index.css';
 import App from './App';
-import MemoStore from './memo/Store';
+
+//　ステート
+let state_value = {
+  counter:0,
+  message:"COUNTER"
+}
+
+//　レデューサー
+function counter(state = state_value, action) {
+  switch (action.type) {
+    case 'INCREMENT':
+    return {
+      counter:state.counter + 1,
+      message:"INCREMENT"
+    };
+    case 'DECREMENT':
+    return {
+      counter:state.counter - 1,
+      message:"DECREMENT"
+    };
+    case 'RESET':
+    return {
+      counter:0,
+      message:"RESET"
+    }
+    default:
+    return state;
+  }
+}
+
+// Redux Persist
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+// パーシストレデューサー
+const persistedReducer = persistReducer(persistConfig, counter);
+
+//　ストア、パーシスター
+let store = createStore(persistedReducer);
+let pstore = persistStore(store);
 
 ReactDOM.render(
-  <Provider store={MemoStore}>
-    <App />
+  <Provider store={store}>
+    <PersistGate loading={<p>loading...</p>} persistor={pstore}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById('root')
 );
